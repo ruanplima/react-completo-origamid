f7a157e6b8c0fb0c55ae07f6143ade60e686e0d2
Ruan Pinheiro <ruanpinheirolima2003@gmail.com>
2026-03-18 15:50:52 -0300

    Aula de Propriedades e Componentes em React

---
+++ componentes/src/footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      Este é o meu Footer
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ componentes/src/header.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Header = () => {
+  return <header>Este é o meu Header</header>;
+};
+
+export default Header;
