3ec1b70d5ff821a55dc01ac4d5eecf3b400b2991
Ruan Pinheiro <ruanpinheirolima2003@gmail.com>
2026-03-26 10:45:11 -0300

    Aprendendo mais sobre o useEffect | React Completo

--- UseEffect/src/App.jsx
+++ UseEffect/src/App.jsx
@@ -2,19 +2,40 @@ import React from 'react';
 import '../../style-global.css';
 
 function App() {
-  const [contar, setContar] = React.useState(0);
+  const [contar, setContar] = React.useState(1);
+  const [dados, setDados] = React.useState(null);
   function handleClick() {
     setContar(contar + 1);
   }
 
   React.useEffect(() => {
-    console.log('Clicou');
+    fetch('https://ranekapi.origamid.dev/json/api/produto/notebook')
+      .then((response) => response.json())
+      .then((response) => setDados(response));
   }, []);
+
+  const preco = dados && Number(dados.preco * contar);
+  const precoFormatado = new Intl.NumberFormat('pt-BR', {
+    style: 'currency',
+    currency: 'BRL',
+  }).format(preco);
 
   return (
     <div>
       <h1>UseEffect</h1>
-      <button onClick={handleClick}>{contar}</button>
+
+      {dados && (
+        <div>
+          <h3>{dados.nome}</h3>
+          <p>{precoFormatado}</p>
+          <img
+            style={{ maxWidth: '300px' }}
+            src={dados.fotos[0].src}
+            alt={dados.fotos[0].titulo}
+          />
+        </div>
+      )}
+      <button onClick={handleClick}>Comprando: {contar}</button>
     </div>
   );
 }
