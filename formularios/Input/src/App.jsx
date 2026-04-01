9802aa1ff7b6007537ede762ebb678c3b7a456ef
Ruan Pinheiro <ruanpinheirolima2003@gmail.com>
2026-04-01 14:38:14 -0300

    Iniciando os estudos em formulários | React Completo

--- formularios/Input/src/App.jsx
+++ formularios/Input/src/App.jsx
@@ -2,9 +2,46 @@ import React from 'react';
 import '../../../style-global.css';
 
 function App() {
+  const [nome, setNome] = React.useState('');
+  const [email, setEmail] = React.useState('');
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log({
+      nome,
+      email,
+    });
+  }
+
   return (
     <div>
       <h1>Input</h1>
+
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="nome">Nome</label>
+        <input
+          id="nome"
+          type="text"
+          name="nome"
+          value={nome}
+          onChange={({ target }) => {
+            setNome(target.value);
+          }}
+        />
+
+        <label htmlFor="email">E-mail</label>
+        <input
+          type="email"
+          name="email"
+          id="email"
+          value={email}
+          onChange={({ target }) => {
+            setEmail(target.value);
+          }}
+        />
+
+        <button>Enviar</button>
+      </form>
     </div>
   );
 }
